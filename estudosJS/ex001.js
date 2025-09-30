f0adb02c75a29004de356df478472c9c3518f2da
Fellipe Almeida <150681795+FellipeAlmeida@users.noreply.github.com>
2025-09-30 16:00:44 -0300

    Delete estudosJS directory

--- estudosJS/ex001.js
+++
@@ -1,7 +0,0 @@
-let notas = [6, 8, 7, 10, 5]
-
-notas.reduce((count, i) => {
-    count+=i
-    console.log(count)
-}, 0)
-console.log(media)
--- estudosJS/ex002.js
+++
@@ -1,6 +0,0 @@
-let notas = [5, 2, 9, 1]
-
-notas.reduce((acum, n) => {
-    acum += n
-    console.log(acum)
-}, 0)
